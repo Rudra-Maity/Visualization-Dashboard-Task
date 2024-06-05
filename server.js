7b4cb1e71084bde2216c78694aad3a5e675b2ee9
unknown <maityrudra228@gmail.com>
2024-06-05 19:59:49 +0530

    c

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./routes/routes');
 const path = require('path');
+const favicon=require('express-favicon');
+const morgan=require('morgan');
 require('dotenv').config(); 
 require('./config/conn')
 
 const app = express();
 
 app.use(express.query());
-app.use(cors());
 app.use(express.json());
-
+app.use(morgan())
 app.use(express.static(path.join(__dirname,'resources','client','build')))
+app.use(favicon(path.join(__dirname, 'resources', 'client', 'build', 'favicon.png')));
 
 app.use('/', apiRoutes);
 app.get('/*', function(req, res) {
